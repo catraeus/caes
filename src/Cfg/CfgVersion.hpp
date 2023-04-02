080a8bc54911d6609238c61e5cdf18407d629f41
Duncan Gray <duncanbranch@gmail.com>
2023-04-02 09:57:54 -0500

    Library version of GIT version increment method.

---
+++ src/Cfg/CfgVersion.hpp
@@ -0,0 +1,24 @@
+
+
+//=================================================================================================
+// Original File Name : CfgVersion.h
+// Original Author    : duncang
+// Creation Date      : May 31, 2012
+// Copyright          : Copyright © 2012 - 2023 by Catraeus and Duncan Gray
+//
+// Description        :
+//    Contains the constants that tell everybody what this thing is.
+//
+//=================================================================================================
+
+#define GIT_MARK     769
+#define VER_MAIN      1
+#define VER_POINT     0
+#define APP_NAME     "hematite"
+#define APP_ID_NM    "hematite"
+#define APP_DESC     "Wave File Analysis Tool"
+#define STR_CPYR     "Copyright © 2012 - 2022 Catraeus and Duncan Gray"
+#define STR_AUTH     "Catraeus and Duncan Gray"
+#define STR_URL      "http://www.catraeus.com"
+#define STR_URL_LBL  "www.catraeus.com"
+
