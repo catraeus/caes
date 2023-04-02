b16dd0800d799975f678e46fbfabd0959a2d2513
Duncan Gray <duncanbranch@gmail.com>
2023-04-02 10:00:23 -0500

    Library Version File has correct library name.

--- src/Cfg/CfgVersion.hpp
+++ src/Cfg/CfgVersion.hpp
@@ -13,14 +13,14 @@
 
 // WARNING Must be one and only included library root in any single project
 
-#define GIT_LIB_MARK     228
-#define VER_LIB_MAIN      1
-#define VER_LIB_POINT     0
-#define APP_LIB_NAME     "hematite"
-#define APP_LIB_ID_NM    "hematite"
-#define APP_LIB_DESC     "Wave File Analysis Tool"
-#define STR_LIB_CPYR     "Copyright © 2012 - 2022 Catraeus and Duncan Gray"
-#define STR_LIB_AUTH     "Catraeus and Duncan Gray"
-#define STR_LIB_URL      "http://www.catraeus.com"
-#define STR_LIB_URL_LBL  "www.catraeus.com"
+#define GIT_LIB_MARK     230
+#define VER_LIB_MAIN       1
+#define VER_LIB_POINT      0
+#define APP_LIB_NAME      "caeslib"
+#define APP_LIB_ID_NM     "caeslib"
+#define APP_LIB_DESC      "Wave File Analysis Tool"
+#define STR_LIB_CPYR      "Copyright © 2012 - 2023 Catraeus and Duncan Gray"
+#define STR_LIB_AUTH      "Catraeus and Duncan Gray"
+#define STR_LIB_URL       "http://www.catraeus.com"
+#define STR_LIB_URL_LBL   "www.catraeus.com"
 
